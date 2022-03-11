9318f54991463c4d4459b9349623e693b5420f9b
jordyf15 <jordyferdian88@gmail.com>
2022-03-11 17:36:47 +0700

    added bson for user model

--- models/user.go
+++ models/user.go
@@ -1,12 +1,12 @@
 package models
 
 type User struct {
-	Id              string           `json:"id"`
-	Username        string           `json:"username"`
-	Fullname        string           `json:"fullname"`
-	Password        string           `json:"password"`
-	Email           string           `json:"email"`
-	ProfilePictures []ProfilePicture `json:"profile_pictures"`
+	Id              string           `json:"id" bson:"_id"`
+	Username        string           `json:"username" bson:"username"`
+	Fullname        string           `json:"fullname" bson:"full_name"`
+	Password        string           `json:"password" bson:"password"`
+	Email           string           `json:"email" bson:"email"`
+	ProfilePictures []ProfilePicture `json:"profile_pictures" bson:"profile_pictures"`
 }
 
 func NewUser(id string, username string, fullname string, password string, email string, profilePictures []ProfilePicture) *User {
